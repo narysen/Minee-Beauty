e2e7d1e358f4e7371d73f731c8d9b52326b64ebc
Sen Nary <sennary@Sens-MacBook-Pro.local>
2026-03-29 15:15:45 +0700

    update backend

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
+
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 
-require("dotenv").config();
 
 const app = express();
 app.use(cors());
